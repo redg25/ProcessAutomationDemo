57bb9815aa06dc728de18e97d332074ad483bbe3
regis <regiscorblin@yahoo.fr>
2021-10-05 11:32:52 +0200

    Re-start git

--- Main_demo.py
+++ Main_demo.py
@@ -1,13 +1,14 @@
 from gead import *
 from ocr_bsd import *
 from folder_demo import get_folder_path
+from config import user,password
 from create_histo import create_histogram
 from generate_report import *
 
 messages_filter = 'SUBJECT "Invoices" UNSEEN'
 
 while True:
-    var = get_email_attachment_demo('****', "****", filters='SUBJECT "Invoices"')
+    var = get_email_attachment_demo(user, password, filters='SUBJECT "Invoices"')
     var.conn()
     var.get_filtered_emails()
     for email in var.emails_ids:
